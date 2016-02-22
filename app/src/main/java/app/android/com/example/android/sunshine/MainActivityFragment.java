93ceffe55a795e43de0d7031a9f1439aaf129f63
Ricardo Candeias <ricardocandeiast@gmail.com>
2016-02-22 17:49:07 +0800

    Lesson 1 (1st App) all files

---
+++ app/src/main/java/app/android/com/example/android/sunshine/MainActivityFragment.java
@@ -0,0 +1,28 @@
+package app.android.com.example.android.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+}
